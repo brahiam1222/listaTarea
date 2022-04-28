adc406cbc64bbe50b4976e0fd0f97f539ca62d2a
brahi <brahi@DESKTOP-8V1T4Q4>
2022-04-28 16:38:11 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const contenido = document.getElementById('contenido');
+
+const agregarTarea = e =>{
+    e.preventDefault();
+    const {value} = e.target.textoTarea;
+    const tarea = document.createElement('div');
+    tarea.classList.add('tarea' , 'borde');
+    tarea.addEventListener('click', cambioEstado);
+    tarea.textContent = value;
+    contenido.prepend(tarea);
+    e.target.reset();
+
+}
+
+const cambioEstado = e =>{
+    e.target.classList.toggle('completado');
+}
+
+
+const ordenado = () =>{
+
+    const pendiente = [];
+    const completado = [];
+    contenido.childNodes.forEach(item =>{
+        if(item.classList.contains('completado')){
+            completado.push(item);
+        }else{
+        
+            pendiente.push(item);
+        
+        };
+        
+        });
+         return [...pendiente , ...completado];
+        
+        }
+
+        const ordenar = () =>{
+
+            ordenado().forEach(item => contenido.appendChild(item));
+            
+            
+            };
+
+        const addBtnEliminar = () =>{
+                
+                const deleteBtn = document.createElement('button');
+                deleteBtn.textContent='x';
+                
+                deleteBtn.addEventListener('click', e=>{
+                    const item = e.target.parentElement;
+                    div.removeChild(item);
+                });
+
+                return deleteBtn;
+
+
+        }
+
+
+
+
