320b9b560d2cc3ab0b22b6cdac1ed18c23016098
Brahiam Steven <brahiam-1222@hotmail.com>
2022-04-30 13:55:53 -0500

    Terminado js

--- script.js
+++ script.js
@@ -1,62 +1,98 @@
 const contenido = document.getElementById('contenido');
+contenido.setAttribute('display', 'grid', 'grid-template-columns' , '2fr 1fr');
 
 const agregarTarea = e =>{
     e.preventDefault();
     const {value} = e.target.textoTarea;
     const tarea = document.createElement('div');
+    tarea.setAttribute('id', 'listTarea');
+    // const idTarea = document.getElementById('listTarea');
     const boton = document.createElement('button');
+    boton.textContent = 'x';
     tarea.classList.add('tarea' , 'borde');
-    tarea.addEventListener('click', cambioEstado);
+    tarea.addEventListener( 'click', cambioEstado);
+    boton.addEventListener('click', eliminar);
+    boton.addEventListener('click', Eliminado);
     tarea.textContent = value;
-    contenido.prepend(tarea, boton);
+    tarea.prepend(boton);
+    contenido.prepend(tarea);
+    
     e.target.reset();
-
+    
+    // console.log(contenido);
 }
 
 const cambioEstado = e =>{
     e.target.classList.toggle('completado');
 }
 
+const eliminar = e =>{
+
+    e.target.id = 'eliminar';
+
+    
+}
+
+
+
+const Eliminado = () =>{
+    
+    contenido.childNodes.forEach(recurso =>{
+        recurso.childNodes.forEach(item =>{
+            if(item.id === 'eliminar'){
+                item.parentNode.remove();
+            }
+        })
+                  
+    });
+    console.log(Eliminado);
+    
+    
+    
+}
+
+
 
 const ordenado = () =>{
-
+    
     const pendiente = [];
     const completado = [];
     contenido.childNodes.forEach(item =>{
         if(item.classList.contains('completado')){
             completado.push(item);
         }else{
-        
+            
             pendiente.push(item);
-        
+            
         };
         
-        });
-         return [...pendiente , ...completado];
-        
-        }
+    });
+    return [...pendiente , ...completado];
+    
+}
 
-        const ordenar = () =>{
-
-            ordenado().forEach(item => contenido.appendChild(item));
+const ordenar = () =>{
+    
+    ordenado().forEach(item => contenido.appendChild(item));
             
             
-            };
+};
 
-        const addBtnEliminar = () =>{
-                
-                const deleteBtn = document.createElement('button');
-                deleteBtn.textContent='x';
-                
-                deleteBtn.addEventListener('click', e=>{
-                    const item = e.target.parentElement;
-                    div.removeChild(item);
-                });
+const addBtnEliminar = () =>{
+    
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent='x';
+    
+    deleteBtn.addEventListener('click', e=>{
+        const item = e.target.parentElement;
+        div.removeChild(item);
+    });
+    
+    return deleteBtn;
+    
+    
+}
 
-                return deleteBtn;
-
-
-        }
 
 
 
